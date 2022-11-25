b749e8d5a9a22619a0bc011e578e52a7c3ba6256
Ashim-mollick <ashimmollick46@gmail.com>
2022-11-25 19:55:12 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ue2o2me.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run() {
+    try {
+        const catogoryOptionCollection = client.db('headphone').collection('catagory');
+
+
+
+        app.get('/catagory', async (req, res) => {
+
+            const query = {};
+            const options = await catogoryOptionCollection.find(query).toArray();
+            res.send(options);
+        });
+
+
+
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
+
+app.get('/', async (req, res) => {
+    res.send('headphone server is running');
+})
+
+app.listen(port, () => console.log(`Headphone server running on ${port}`))
